c3547ff47bbb1b1362a76493b6ccb5521d869ce4
Ojaswin Kaushik <ojaswin.kaushik@oneimpression.io>
2025-05-12 15:23:23 +0530

    animation

--- app/contactUs/page.jsx
+++ app/contactUs/page.jsx
@@ -3,11 +3,17 @@ import React from 'react';
 import ContactUsForm from './components/ContactUsForm';
 import ContactDetails from './components/ContactDetails';
 import ContactUsFooter from '@/components/ContactUsFooter';
+import FallingLeaves from '@/components/FallingLeaves';
+import styles from './index.module.css';
+import clsx from 'clsx';
 
 const ContactUs = () => {
   return (
     <div>
       <ShopHeader />
+      <div className={clsx('relative', styles.margin)}>
+        <FallingLeaves />
+      </div>
       <div className="grid grid-cols-2 gap-x-4">
         <ContactUsForm></ContactUsForm>
         <ContactDetails />
---
+++ components/FallingLeaves/index.jsx
@@ -0,0 +1,174 @@
+'use client';
+import React, { useEffect, useRef } from 'react';
+import styles from './index.module.css';
+import { Dancing_Script } from 'next/font/google';
+import clsx from 'clsx';
+
+const dancing = Dancing_Script({
+  subsets: ['latin'],
+  weight: ['400', '700'], // optional
+  display: 'swap', // recommended
+});
+const FallingLeaves = () => {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    class LeafScene {
+      constructor(el) {
+        this.viewport = el;
+        this.world = document.createElement('div');
+        this.leaves = [];
+
+        this.options = {
+          numLeaves: 20,
+          wind: {
+            magnitude: 1.2,
+            maxSpeed: 12,
+            duration: 300,
+            start: 0,
+            speed: 0,
+          },
+        };
+
+        this.width = this.viewport.offsetWidth;
+        this.height = this.viewport.offsetHeight;
+        this.timer = 0;
+
+        this._resetLeaf = (leaf) => {
+          leaf.x = this.width * 2 - Math.random() * this.width * 1.75;
+          leaf.y = -10;
+          leaf.z = Math.random() * 200;
+          if (leaf.x > this.width) {
+            leaf.x = this.width + 10;
+            leaf.y = (Math.random() * this.height) / 2;
+          }
+          if (this.timer === 0) {
+            leaf.y = Math.random() * this.height;
+          }
+
+          leaf.rotation.speed = Math.random() * 10;
+          const randomAxis = Math.random();
+          if (randomAxis > 0.5) {
+            leaf.rotation.axis = 'X';
+          } else if (randomAxis > 0.25) {
+            leaf.rotation.axis = 'Y';
+            leaf.rotation.x = Math.random() * 180 + 90;
+          } else {
+            leaf.rotation.axis = 'Z';
+            leaf.rotation.x = Math.random() * 360 - 180;
+            leaf.rotation.speed = Math.random() * 3;
+          }
+
+          leaf.xSpeedVariation = Math.random() * 0.8 - 0.4;
+          leaf.ySpeed = Math.random() + 1.5;
+
+          return leaf;
+        };
+
+        this._updateLeaf = (leaf) => {
+          const leafWindSpeed = this.options.wind.speed(
+            this.timer - this.options.wind.start,
+            leaf.y
+          );
+
+          const xSpeed = leafWindSpeed + leaf.xSpeedVariation;
+          leaf.x -= xSpeed;
+          leaf.y += leaf.ySpeed;
+          leaf.rotation.value += leaf.rotation.speed;
+
+          let t = `translateX(${leaf.x}px) translateY(${leaf.y}px) translateZ(${leaf.z}px) rotate${leaf.rotation.axis}(${leaf.rotation.value}deg)`;
+          if (leaf.rotation.axis !== 'X') {
+            t += ` rotateX(${leaf.rotation.x}deg)`;
+          }
+          leaf.el.style.transform = t;
+
+          if (leaf.x < -10 || leaf.y > this.height + 10) {
+            this._resetLeaf(leaf);
+          }
+        };
+
+        this._updateWind = () => {
+          if (
+            this.timer === 0 ||
+            this.timer > this.options.wind.start + this.options.wind.duration
+          ) {
+            this.options.wind.magnitude =
+              Math.random() * this.options.wind.maxSpeed;
+            this.options.wind.duration =
+              this.options.wind.magnitude * 50 + (Math.random() * 20 - 10);
+            this.options.wind.start = this.timer;
+
+            const screenHeight = this.height;
+
+            this.options.wind.speed = function (t, y) {
+              const a =
+                ((this.magnitude / 2) * (screenHeight - (2 * y) / 3)) /
+                screenHeight;
+              return (
+                a *
+                  Math.sin(
+                    ((2 * Math.PI) / this.duration) * t + (3 * Math.PI) / 2
+                  ) +
+                a
+              );
+            };
+          }
+        };
+
+        this.init = () => {
+          for (let i = 0; i < this.options.numLeaves; i++) {
+            const leaf = {
+              el: document.createElement('div'),
+              x: 0,
+              y: 0,
+              z: 0,
+              rotation: { axis: 'X', value: 0, speed: 0, x: 0 },
+              xSpeedVariation: 0,
+              ySpeed: 0,
+              path: { type: 1, start: 0 },
+              image: 1,
+            };
+            this._resetLeaf(leaf);
+            this.leaves.push(leaf);
+            this.world.appendChild(leaf.el);
+          }
+
+          this.world.className = styles['leaf-scene'];
+          this.viewport.appendChild(this.world);
+
+          this.world.style.perspective = '400px';
+
+          window.onresize = () => {
+            this.width = this.viewport.offsetWidth;
+            this.height = this.viewport.offsetHeight;
+          };
+        };
+
+        this.render = () => {
+          this._updateWind();
+          for (let i = 0; i < this.leaves.length; i++) {
+            this._updateLeaf(this.leaves[i]);
+          }
+          this.timer++;
+          requestAnimationFrame(this.render.bind(this));
+        };
+      }
+    }
+
+    const container = containerRef.current;
+    const leaves = new LeafScene(container);
+    leaves.init();
+    leaves.render();
+  }, []);
+
+  return (
+    <div
+      className={clsx(styles['falling-leaves'], dancing.className)}
+      ref={containerRef}
+    >
+      Every thread tells a story — woven by hand, crafted by heart.
+    </div>
+  );
+};
+
+export default FallingLeaves;
