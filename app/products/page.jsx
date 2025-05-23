45bc979a8382c9bcf986609c9942747f1ebd0b0a
Ojaswin Kaushik <ojaswin.kaushik@oneimpression.io>
2025-05-23 17:48:37 +0530

    pagination footer

--- app/products/page.jsx
+++ app/products/page.jsx
@@ -2,6 +2,7 @@ import ShopHeader from '@/components/ShopHeader';
 import React from 'react';
 import CategoryFilter from './components/CategoryFilter';
 import ContactUsFooter from '@/components/ContactUsFooter';
+import PaginationFooter from '@/components/PaginationFooter';
 
 const Shop = () => {
   return (
@@ -9,7 +10,9 @@ const Shop = () => {
       <ShopHeader />
       <div className="grid grid-cols-[200px_1fr] px-[80px] gap-x-4 py-[40px]">
         <CategoryFilter />
-        <div></div>
+        <div className="relative">
+          <PaginationFooter />
+        </div>
       </div>
       <ContactUsFooter />
     </div>
--- utls/index.js
+++ utls/index.js
@@ -4,3 +4,16 @@ export const toastTypes = {
   WARNING: 'warning',
   INFO: 'info',
 };
+
+export function* range(start, end, step = 1) {
+  let i;
+  if (step > 0) {
+    for (i = start; i < end; i += step) {
+      yield i;
+    }
+  } else {
+    for (i = start; i > end; i -= step) {
+      yield i;
+    }
+  }
+}
