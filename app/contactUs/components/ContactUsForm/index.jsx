4ac182a039956de3744acd122e817a680eeba40a
Ojaswin Kaushik <oceanorsky1@gmail.com>
2025-08-04 04:31:57 +0530

    minor

--- app/contactUs/components/ContactUsForm/index.jsx
+++ app/contactUs/components/ContactUsForm/index.jsx
@@ -1,13 +1,39 @@
-import React from 'react';
+'use client';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import useToast from '@/store/useToast';
+import { toastTypes } from '@/utls';
 
 const ContactUsForm = () => {
+  const form = useRef();
+  const { addToast } = useToast();
+  const service_id = 'service_w89xl9f';
+  const template_id = 'template_7154r0q';
+  const public_key = 'Fx45D07KIG9rWyylp';
+
+  const sendEmail = async (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(service_id, template_id, form.current, public_key).then(
+      () => {
+        form.current.reset();
+        addToast('Message sent successfully!', toastTypes.SUCCESS);
+      },
+      (error) => {
+        console.log('Email send error:', error.text);
+        addToast('Failed to send message.', toastTypes.ERROR);
+      }
+    );
+  };
+
   return (
     <div className="flex flex-col items-start justify-center h-full p-6">
       <h1 className="text-xl font-bold mb-4">Tell Us Your Project</h1>
-      <form className="w-full max-w-lg">
+      <form className="w-full max-w-lg" ref={form} onSubmit={sendEmail}>
         <div className="grid grid-cols-2 gap-8">
           <input
             type="text"
+            name="name"
             id="name"
             placeholder="Name*"
             className="appearance-none w-full p-3 text-sm text-gray-700 bg-gray-100 leading-tight focus:outline-none focus:-outline"
@@ -16,6 +42,7 @@ const ContactUsForm = () => {
           <input
             type="phone"
             id="phone"
+            name="phone"
             placeholder="Phone*"
             className=" appearance-none w-full p-3 text-sm text-gray-700 bg-gray-100 leading-tight focus:outline-none focus:-outline"
           />
@@ -23,6 +50,7 @@ const ContactUsForm = () => {
           <input
             type="email"
             id="email"
+            name="email"
             placeholder="Email*"
             className=" appearance-none w-full p-3 text-sm text-gray-700 bg-gray-100 leading-tight focus:outline-none focus:-outline"
           />
@@ -31,6 +59,7 @@ const ContactUsForm = () => {
             type="text"
             id="subject"
             placeholder="Subject*"
+            name="subject"
             className=" appearance-none w-full p-3 text-sm text-gray-700  bg-gray-100 leading-tight focus:outline-none focus:-outline"
           />
         </div>
@@ -38,6 +67,7 @@ const ContactUsForm = () => {
         <div className="my-4">
           <textarea
             id="message"
+            name="message"
             placeholder="Message*"
             rows={6}
             className=" appearance-none w-full p-3 text-sm text-gray-700 bg-gray-100 leading-tight focus:outline-none focus:-outline"
