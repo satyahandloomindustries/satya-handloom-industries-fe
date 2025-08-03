3704a47b772e7adcf43ed9d3cd0ffb0d274cc8e0
Ojaswin Kaushik <oceanorsky1@gmail.com>
2025-08-04 05:10:31 +0530

    cover resize 2

--- components/CarouselHomepage/index.jsx
+++ components/CarouselHomepage/index.jsx
@@ -22,44 +22,43 @@ const CollectionLink = ({ label = '' }) => {
 const CarouselHomepage = () => {
   return (
     <div className="flex overflow-x-scroll space-x-2 overflow-y-hidden h-[450] px-4 scrollbar-hide">
-      <div className="flex-shrink-0 relative group overflow-hidden">
+      <div className="flex-shrink-0 relative group overflow-hidden w-[400px] h-[450px]">
         <Image
           src="/traditional-macrame-composition-indoors_317x449.jpg"
           className="group-hover:scale-110 group-hover:opacity-90 transition-transform transform duration-1000"
           alt="assembling-advent3"
-          width={400}
-          height={400}
+          layout="fill"
+          objectFit="cover"
         />
         <CollectionLink label="Ethnic" />
       </div>
-      <div className="flex-shrink-0 group overflow-hidden relative">
+      <div className="flex-shrink-0 group overflow-hidden relative w-[400px] h-[450px]">
         <Image
           src="/Reviving_the_Beauty_of_Traditional_Practices_317x449.jpeg"
           className="group-hover:scale-110 group-hover:opacity-90 transition-transform transform duration-1000"
           alt="assembling-advent2"
-          width={400}
-          height={400}
+          objectFit="cover"
+          layout="fill"
         />
         <CollectionLink label="Artistic" />
       </div>
-      <div className="flex-shrink-0 group overflow-hidden relative">
+      <div className="flex-shrink-0 group overflow-hidden relative w-[400px] h-[450px] ">
         <Image
           src="/young-woman-using-macrame-technique.jpg"
           className="group-hover:scale-110 group-hover:opacity-90 transition-transform transform duration-1000"
           alt="assembling-advent4"
           objectFit="cover"
-          width={400}
-          height={400}
+          layout="fill"
         />
         <CollectionLink label="Vibrant" />
       </div>
 
-      <div className="flex-shrink-0 group overflow-hidden relative">
+      <div className="flex-shrink-0 group overflow-hidden relative w-[400px] h-[450px]">
         <Image
           src="/hands-assembling-advent-wreath_317x449.jpg"
           alt="assembling-advent1"
-          width={400}
-          height={400}
+          layout="fill"
+          objectFit="cover"
           className="group-hover:scale-110 group-hover:opacity-90 transition-transform transform duration-1000"
         />
         <CollectionLink label="Timeless" />
