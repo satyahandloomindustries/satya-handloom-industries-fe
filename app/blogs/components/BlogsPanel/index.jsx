ca3d8d5693b0b1f97ac1d7b4689fde701c3256b5
Ojaswin Kaushik <oceanorsky1@gmail.com>
2025-10-04 04:02:26 +0530

    responsive to key press

--- app/blogs/components/BlogsPanel/index.jsx
+++ app/blogs/components/BlogsPanel/index.jsx
@@ -1,33 +1,47 @@
 'use client';
 import Searchbar from '@/components/Searchbar';
 import useBlogs from '@/store/useBlogs';
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect } from 'react';
 
-const BlogsPanel = ({ blogs =[]}) => {
-  const {setBlogs , currentBlog , setCurrentBlog , filteredBlogs , setFilteredBlogs } = useBlogs()
+const BlogsPanel = ({ blogs = [] }) => {
+  const { setBlogs, currentBlog, setCurrentBlog, filteredBlogs, setFilteredBlogs , currentIndex} = useBlogs()
 
-  
-  useEffect(()=>{
+
+  useEffect(() => {
     setBlogs(blogs);
     setCurrentBlog(0)
-  },[blogs])
-  
-    
-  
+  }, [blogs])
+
+
+  const handleKeydown = useCallback((event)=>{
+
+    switch(event.key){
+
+      case "ArrowDown": {
+        currentIndex + 1 >= filteredBlogs.length ? setCurrentBlog(0) : setCurrentBlog(currentIndex + 1);
+        break;
+      }
+      case "ArrowUp": {
+        currentIndex - 1 < 0 ? setCurrentBlog(filteredBlogs.length - 1) : setCurrentBlog(currentIndex - 1);
+        break
+      }
+    }
+  } , [currentIndex , filteredBlogs , setCurrentBlog])
+
   return (
     <div
       className="w-[300px] text-white backdrop-blur p-4 flex flex-col"
       style={{ backgroundColor: 'rgb(175, 159, 93)' }}
     >
       <div className="text-center text-lg">Thread Tales</div>
-      <Searchbar onSearchComplete={setFilteredBlogs}/>
+      <Searchbar onSearchComplete={setFilteredBlogs} />
 
-      {! currentBlog ? <p>No threads to show</p>:<ol className="list-decimal pl-6 h-full mt-6 space-y-2 overflow-y-auto scrollbar-rounded scrollbar-thin scrollbar-thumb-gray-100 scrollbar-track-transparent">        {filteredBlogs.map((item, index) => (
+      {!currentBlog ? <p>No threads to show</p> : <ol className="list-decimal pl-6 h-full mt-6 space-y-2 overflow-y-auto scrollbar-rounded scrollbar-thin scrollbar-thumb-gray-100 scrollbar-track-transparent outline-none" tabIndex={0} onKeyDown={handleKeydown}>        {filteredBlogs.map((item, index) => (
         <li
           className={`p-1 tracking-wide cursor-pointer rounded text-sm ${item.id === currentBlog?.id ? 'bg-black/10 backdrop-blur' : ''}`}
           key={item.id}
         >
-          <button className='w-full text-left active:scale-95 transition-all duration-200' onClick={setCurrentBlog.bind(null, index)}>
+          <button className='w-full text-left active:scale-95 transition-all duration-200 focus:outline-none outline-none' onClick={setCurrentBlog.bind(null, index)}>
             {item.title}
 
           </button>
--- store/useBlogs.js
+++ store/useBlogs.js
@@ -6,12 +6,13 @@ const useBlogs = create((set, get) => ({
     blogs: [],
     filteredBlogs: [],
     currentBlog: null,
-    setCurrentBlog: (index) => set({ currentBlog: get().blogs?.[index] ?? null}),
+    currentIndex : null, 
+    setCurrentBlog: (index) => set({ currentBlog: get().blogs?.[index] ?? null , currentIndex : get().blogs?.[index] ? index : null}),
     setBlogs: (blogs) => set({ blogs  , filteredBlogs: blogs}),
     setFilteredBlogs: () => {
         const {query} = useVoiceSearch.getState()
         const filteredBlogs = get().blogs.filter((blog) => matchesSearch(blog.title , query))
-        set({ filteredBlogs , currentBlog : filteredBlogs?.[0] ?? null})
+        set({ filteredBlogs , currentBlog : filteredBlogs?.[0] ?? null , currentIndex : get().blogs?.[0] ? 0 : null})
     },
 }));
 
