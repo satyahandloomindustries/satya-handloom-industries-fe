156f6132a97ff6d06196ebe91b29a5bebb8f3ff5
Ojaswin Kaushik <oceanorsky1@gmail.com>
2025-10-17 15:39:39 +0530

    paste feature handled

--- app/login/components/Otp/index.jsx
+++ app/login/components/Otp/index.jsx
@@ -1,5 +1,5 @@
 "use client"
-import { verifyDigits } from "@/utls";
+import { evd, pasteOtp, verifyDigits } from "@/utls";
 import { useEffect, useRef } from "react"
 
 const Otp = ({ numberOfBoxes = 4  , inputs = new Array(4).fill("")  , setInputs=()=>{} , visible = false}) => {
@@ -58,11 +58,31 @@ const Otp = ({ numberOfBoxes = 4  , inputs = new Array(4).fill("")  , setInputs=
         }
 
     }
+    const handlePaste = evd((event)=>{
+        try {
+
+        const pastedData = pasteOtp(event).slice(0 ,numberOfBoxes);
+        const newOtp = pastedData.padEnd(numberOfBoxes).split("");
+        setInputs(newOtp)
+        newOtp.forEach((char, i) => {
+            if (refs.current[i]) {
+                refs.current[i].value = char;
+            }
+          });
+        
+           // focus last filled input
+        const lastIndex = Math.min(pastedData.length - 1, numberOfBoxes - 1);
+        handleOtpFocus(lastIndex);
+        }
+        catch(err){}
+    })
+
+      
 
     if(!visible) return null
     return <div className="flex items-start justify-center flex-col">
         <p className="text-sm mb-2 font-medium text-gray-700">Enter the verification code</p>
-        <div className="grid gap-x-4 grid-cols-[repeat(4,50px)] place-items-center mb-2">
+        <div className="grid gap-x-4 grid-cols-[repeat(4,50px)] place-items-center mb-2" onPaste={handlePaste}>
             {inputs.map((i, index) =>
 
                 <input name={index.toString()} autoComplete="off" type="text" ref={(i)=> refs.current.push(i)} value={i} className="border border-gray-300  w-12 h-12 text-center" key={index} onChange={(event)=>handleChange(event ,  index)} onKeyDown={(event)=>handleKeyDown(event , index)}/>
