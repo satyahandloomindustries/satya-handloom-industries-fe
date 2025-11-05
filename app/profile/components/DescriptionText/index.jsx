91b7a28c7a8d381684f75a8d26b61e9297d5203a
Ojaswin Kaushik <oceanorsky1@gmail.com>
2025-11-05 11:15:35 +0530

    minor

--- app/profile/components/DescriptionText/index.jsx
+++ app/profile/components/DescriptionText/index.jsx
@@ -2,8 +2,8 @@ import { TiDelete } from "react-icons/ti";
 
 const DescriptionText = ({item , onClose = ()=>{}}) => {    
     
-    return <div className="flex flex-row w-full p-3 bg-gray-100 items-center">
-        <div className="w-full">
+    return <div className="flex flex-row w-full p-3 bg-gray-100 items-start justify-center">
+        <div className="w-full text-sm">
             {item}
         </div>
         <TiDelete onClick={()=>onClose(item)} className="cursor-pointer"/>
--- components/Tag/index.jsx
+++ components/Tag/index.jsx
@@ -9,7 +9,7 @@ const tagVariants = {
 const Tag = ({ label = '', variant = tagVariants.SHI_BROWN , onClose = null , mainClassname='absolute top-5 left-5' }) => {
   return (
     <div
-      className={`text-xs text-white ${variant} w-min py-1 px-3 rounded-2xl ${mainClassname}`}
+      className={`text-xs text-white ${variant} w-fit py-1 px-3 rounded-2xl ${mainClassname}`}
     >
       {label}
       {onClose && <TiDelete className="text-white ml-2 cursor-pointer text-base" onClick={onClose}/>}
