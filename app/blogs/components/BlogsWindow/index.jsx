65991ad067eefb79a0a5a58b97125c64a910989e
Ojaswin Kaushik <oceanorsky1@gmail.com>
2025-11-26 13:15:50 +0530

    return statement

--- app/blogs/components/BlogsWindow/index.jsx
+++ app/blogs/components/BlogsWindow/index.jsx
@@ -6,7 +6,7 @@ import useBlogs from '@/store/useBlogs';
 
 const BlogsWindow = () => {
   const { currentBlog = {} } = useBlogs();
-
+  
   if (!currentBlog) return <p>Oops! No thread to read</p>;
   return (
     <div className="flex-1 h-full bg-gray-100 relative">
@@ -16,7 +16,7 @@ const BlogsWindow = () => {
       >
         <h1 className="text-center text-2xl">{currentBlog.title}</h1>
         {currentBlog.description.map((item)=>{
-          <p className='py-6'>{item}</p>
+          return <p className='py-6'>{item}</p>
         })}
       </div>
       <div className="bg-gray-200 w-full h-[100px] absolute bottom-0">
