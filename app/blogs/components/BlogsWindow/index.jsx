22d2e8f183351836c9b44509d2da627b8e70a207
Chirag <chiragagarwal532@gmail.com>
2025-11-26 12:59:06 +0530

    blog

--- app/blogs/components/BlogsWindow/index.jsx
+++ app/blogs/components/BlogsWindow/index.jsx
@@ -15,7 +15,9 @@ const BlogsWindow = () => {
         style={{ height: 'calc(100%  - 100px)' }}
       >
         <h1 className="text-center text-2xl">{currentBlog.title}</h1>
-        <div className="py-6">{currentBlog.description}</div>
+        {currentBlog.description.map((item)=>{
+          <p className='py-6'>{item}</p>
+        })}
       </div>
       <div className="bg-gray-200 w-full h-[100px] absolute bottom-0">
         <div className="relative w-full h-full">
--- components/ContactUsFooter/index.jsx
+++ components/ContactUsFooter/index.jsx
@@ -11,11 +11,11 @@ import SocialIconDisplay from '../SocialIconDisplay';
 export const contactUs = [
   {
     Icon: HomeOutlinedIcon,
-    label: '4710-4890 Breckinridge USA',
+    label: 'Khandak Bazar, Meerut (U.P)',
   },
   {
     Icon: MailOutlinedIcon,
-    label: 'demo@yourdomain.com',
+    label: 'satyahandloomfabrics1@gmail.com',
   },
   {
     Icon: PhoneOutlinedIcon,
@@ -36,7 +36,7 @@ const information = [
 const ContactUsFooter = () => {
   return (
     <footer className="bg-gray-100 py-20 px-20 mt-10">
-      <div className="grid grid-cols-[1.5fr_repeat(3,max-content)] gap-x-4">
+      <div className="grid grid-cols-[repeat(4,1fr)] gap-x-4">
         <div className="mt-2">
           <ShiLogo />
           <p className="text-gray-600 text-sm ml-1 mt-2">
